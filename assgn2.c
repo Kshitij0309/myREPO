92cafed4bfdd25179798daeddf964dd7f6b0eb86
Kshitij0309 <kshitijm0309@gmail.com>
2024-09-08 15:59:01 +0530

    added to take name twice

--- assgn2.c
+++ assgn2.c
@@ -1,1 +1,13 @@
 
+
+#include <stdio.h>
+void main()
+{
+  int rollno;
+  char name;
+  printf("\n Enter your rollno:");
+  scanf("%d",&rollno);
+  printf("\n Enter your name twice:");
+  scanf("%c",&name);
+  printf("\n enter the roll no and name");
+}

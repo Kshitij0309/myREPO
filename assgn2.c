e003a97f7b965763272ee95520cd3abd2fdf8744
Kshitij0309 <kshitijm0309@gmail.com>
2024-09-08 16:05:44 +0530

    removed roll no

--- assgn2.c
+++ assgn2.c
@@ -5,8 +5,8 @@ void main()
 {
   int rollno;
   char name;
-  printf("\n Enter your rollno:");
-  scanf("%d",&rollno);
+ 
+  
   printf("\n Enter your name twice:");
   scanf("%c",&name);
   printf("\n enter the roll no and name");
